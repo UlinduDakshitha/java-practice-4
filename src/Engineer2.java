39d55658020e41e5a1740a28a0d6080907184a21
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-11-18 17:56:27 +0530

    update

--- src/Engineer2.java
+++ src/Engineer2.java
@@ -1,2 +1,16 @@
 public class Engineer2 {
+
+    public static void main(String[] args) {
+
+        Engineer[] engineers={
+                new Engineer("Mr.kamal","Civil Engineer",85000.00),
+                new Engineer("Mr.Siridasa","Planning Engineer",80000.00),
+                new Engineer("Mr.Sugathadasa","Electrical Engineer",95000.00),
+                new Engineer("Mrs.Nayani","Planning Engineer",75000.00)
+
+        };
+
+
+
+    }
 }
