2f115925cab152337db63e040f5a3fead40dc737
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-11-18 15:57:31 +0530

    update

--- src/TeacherDetails.java
+++ src/TeacherDetails.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class TeacherDetails {
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in );
+
+        Teacher[] teacher=new Teacher[3];
+
+        for(int i=0;i<teacher.length;i++){
+
+            System.out.print("Enter your Name:");
+            String name=sc.next();
+
+            System.out.print("Enter your age:");
+            int age= sc.nextInt();
+
+            System.out.print("Enter your salary");
+            double salary=sc.nextDouble();
+
+
+        }
+    }
 }
