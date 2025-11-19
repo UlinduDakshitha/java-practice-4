7936a520c5f833206054ce5154bae30ee0343e96
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-11-19 10:37:50 +0530

    update

--- src/PatientAll.java
+++ src/PatientAll.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class PatientAll {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+       Patient patient[]=new Patient[0];
+
+        for (int i=0;i< patient.length;i++){
+
+
+            System.out.print("Enter your name:");
+            String name= sc.next();
+
+            System.out.print("Enter your age:");
+            int age=sc.nextInt();
+
+            System.out.print("Enter your address:");
+            String address=sc.next();
+
+            System.out.print("Enter your Status:");
+            String status=sc.next();
+
+
+        }
+
+    }
 }
