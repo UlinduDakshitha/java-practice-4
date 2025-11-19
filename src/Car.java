a420f4f69429a737c7e0c1a6b39118e1110c8430
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-11-19 08:57:29 +0530

    update

--- src/Car.java
+++ src/Car.java
@@ -1,16 +1,16 @@
 public class Car extends Van{
 
     public void start(int x){
-        System.out.print(getModel()+getColor()+getTransmission());
+        System.out.print(getModel()+""+getColor()+""+getTransmission());
         System.out.print(getModel()+"is Started");
     }
     public void start(String b){
-        System.out.print(getModel()+getColor()+getTransmission());
+        System.out.print(getModel()+""+getColor()+""+getTransmission());
         System.out.print(getModel()+"is Started");
     }
 
     public void start(double x){
-        System.out.print(getModel()+getColor()+getTransmission());
+        System.out.print(getModel()+""+getColor()+""+getTransmission());
         System.out.print(getModel()+"is Started");
     }
 
