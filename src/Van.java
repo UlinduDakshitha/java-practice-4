145b1fe5236752d660d60a785c0ee464736b8d9a
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-11-19 09:03:13 +0530

    update

--- src/Van.java
+++ src/Van.java
@@ -1,10 +1,10 @@
 public class Van extends Vehicle{
 
     public void start(){
-        System.out.print(getModel());
-        System.out.print(getColor());
-        System.out.print(getTransmission());
-        System.out.println(getModel()+"is started");
+        System.out.println(getModel());
+        System.out.println(getColor());
+        System.out.println(getTransmission());
+        System.out.println(getModel()+""+"is started");
     }
 
 }
