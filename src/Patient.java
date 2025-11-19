fbeb6331104a89538a0cde6618c59586a83134f6
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-11-19 10:21:18 +0530

    Patient class completed

--- src/Patient.java
+++ src/Patient.java
@@ -1,3 +1,38 @@
 public class Patient {
+private String name;
+private int age;
+private String address;
+private String status;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
