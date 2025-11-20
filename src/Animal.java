9b4e4fdb9803d3d8025033ea4452f61ac10d226a
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-11-20 20:11:56 +0530

    update

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Animal {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);;
+
+        Animal2 animal2=new Animal2();
+        Animal2 animal3=new Animal2("Bear",4,"Black");
+
+       Animal2[] animal4=new Animal2[2];
+
+       for (int i=0;i< animal4.length;i++){
+
+
+       }
+
+
+    }
 }
--- src/Animal2.java
+++ src/Animal2.java
@@ -1,2 +1,40 @@
 public class Animal2 {
+    private String name;
+    private int legs;
+    private String color;
+
+
+    public Animal2() {
+        System.out.println("Welcome to the Animal 0bjects");
+    }
+
+    public Animal2(String name,int legs,String color) {
+        this.name = name;
+        this.legs=legs;
+        this.color=color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public void setLegs(int legs) {
+        this.legs = legs;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
